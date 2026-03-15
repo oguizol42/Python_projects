853cd70a15fedb2af7c5031fd96ffbd83dfc6a24
Olivier Guizol <oguizol@c1r2p3.42nice.fr>
2026-03-15 19:03:51 +0100

    Module 04 ex4

--- 04_Module_04/ex0/ft_ancient_text.py
+++ 04_Module_04/ex0/ft_ancient_text.py
@@ -1,29 +1,26 @@
 def main() -> None:
     """Read an Ancient Fragment"""
     file_name: str = "ancient_fragment.txt"
+    reading: int
     try:
         print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===")
         print()
         old_document = open(file_name, "r")
-        old_document.close()
         print(f"Accessing Storage Vault: {file_name}")
         print("Connection established...")
+        print()
+        print("RECOVERED DATA:")
+        reading = old_document.read()
+        print(reading)
+
+        old_document.close()
+        print()
+        print("Data recovery complete. Storage unit disconnected")
     except FileNotFoundError:
-        print("You stupid idiot, don't you know that this file doesn't exist ?")
+        print(
+            "You stupid idiot, don't you know that this file doesn't exist ?"
+        )
+
 
 if __name__ == "__main__":
     main()
-
-
-# $> python3 ft_ancient_text.py
-# === CYBER ARCHIVES - DATA RECOVERY SYSTEM ===
-
-# Accessing Storage Vault: ancient_fragment.txt
-# Connection established...
-
-# RECOVERED DATA:
-# [FRAGMENT 001] Digital preservation protocols established 2087
-# [FRAGMENT 002] Knowledge must survive the entropy wars
-# [FRAGMENT 003] Every byte saved is a victory against oblivion
-
-# Data recovery complete. Storage unit disconnected
