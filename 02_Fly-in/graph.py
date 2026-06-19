231feec8ae915ef1106560bbaf9853bbfd66219a
Olivier Guizol <olivier@localhost.localdomain>
2026-06-19 18:09:21 +0200

    Fly_In graph 190620226

--- 02_Fly-in/graph.py
+++ 02_Fly-in/graph.py
@@ -1,6 +1,65 @@
+from fly_in import Drone_Map
+from typing import Optional
+
+class Map:
+    def __init__(self, map_parsed: Drone_Map) -> None:
+        self.map_parsed: Drone_Map = map_parsed
+        self.size_map: tuple[int, int]
+        self.current_turn: int = 0
+        self.zones_list: list[Map.Zone] = []
+        self.connections_list: list[Map.Connection] = []
+        self.drones_list: list[Map.Drone] = []
+
+    class Zone:
+        def __init__(self) -> None:
+            self.coord_x: int
+            self.coord_y: int
+            self.color: Optional[str] = None
+            self.priority: str = "normal"
+            self.max_drones_allow: int = 1
+            self.drones_in_zone: list[Map.Drone] = []
+            self.connections: list[Map.Connection] = []
+
+    class Connection:
+        def __init__(self) -> None:
+            self.zone_1: Map.Zone
+            self.zone_2: Map.Zone
+            self.max_link_capacity: int = 1
+            self.drones_in_connection: list[Map.Drone] = []
+
+    class Drone:
+        def __init__(self) -> None:
+            self.current_zone: Optional[Map.Zone] = None
+            self.current_connection: Optional[Map.Connection] = None
+            self.destination_zone: Optional[Map.Zone] = None
+            self.remaining_turn: int = 0
+
+        def drone_moving(self) -> None:
+            """Manage Moving of Drone"""
+            # Deplacement des drones
+            # Test si deplacement possible 
+            # Modification des donnes de chaque zone, connections et drones,
+            # en temps reel, au fil des tours
+            # Comptage des tours
+            # Affichage en temps reel
+            pass
+
+        def check_moving(self, dest: "Map.Zone") -> bool:
+            """Check if Drone Moving is Allowed"""
+            return True
+
+    def print_map(self) -> None:
+        """Displaying Graphic Map"""
+        pass
+
+
 # Recupere la map parsée
 
 # class Map
+    # Taille de la map
+    
+    # Nombre de tours deja effectue
+
     # Initialise pour chaque zone (class Zone)
         # Emplacement (coordonnees x, y)
         # Couleur
@@ -32,15 +91,12 @@
         # (cas ou drone dans une connection vers zone restreinte
         # doit etre egale a zero pour que le drone puisse entrer dans la zone)
 
-    # Taille de la map
-
-    # Deplacement des drones
+    # def drone_moving
+        # Deplacement des drones
         # Test si deplacement possible 
         # Modification des donnes de chaque zone, connections et drones,
         # en temps reel, au fil des tours
         # Comptage des tours
         # Affichage en temps reel
-    
-    # Nombre de tours deja effectue
 
     # Affichage
